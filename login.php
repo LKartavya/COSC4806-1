8bcfe272af203990ba7fe5d18ef6c65ff5667885
LKartavya <147118099+LKartavya@users.noreply.github.com>
2025-05-24 03:23:42 +0000

    added validate page

--- login.php
+++ login.php
@@ -4,14 +4,14 @@
   </head>
   <body>
     <h1> Login form</h1>
-    <form action="valtdate.php" method-"post"> 
-      <label for ="username">Username:</label>
-    <br>
-  <input type="text" id="username" name="username">
-    <br>
-    <label for="password">Password:</label> <br>
-    <input type="password" id="password" name="password" > <br> <br>
-    <input type="submit" value="Submit">
+      <form action="valitdate.php" method-"post"> 
+      <label for ="username">Username:</label> <br>
+      <input type="text" id="username" name="username">
+      <br>
+      <label for="password">Password:</label> <br>
+      <input type="password" id="password" name="password" > 
+      <br> <br>
+      <input type="submit" value="Submit">
     </form>
   </body>
 
---
+++ validate.php
@@ -0,0 +1,20 @@
+<?php
+  session_start();
+
+  $valid_username = "mike";
+  $valid_password = "password";
+
+  $username = $_REQUEST[' username'];
+  $password = $_REQUESTI['passwor'];
+  
+  if ($valid_username = Susername && $valid_password = $password) ‹
+    header ('location: /');
+  } else {
+    if (lisset($_SESSION[' failed_attempts'] ))
+      $_SESSION['failed_attempts'] = 1;
+    } else {
+      $_SESSION['failed_attempts'] = $_SESSION[' failed _attempts'] + 1;
+    }
+      echo "This is unsuccessful attempt number ". $_SESSION['failed_attempts'];
+}
+?>
