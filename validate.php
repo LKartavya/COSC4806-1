e65a6328030568b4ed3eb9dd96e79cc65bea4b7d
LKartavya <147118099+LKartavya@users.noreply.github.com>
2025-05-24 03:35:07 +0000

    changes

--- validate.php
+++ validate.php
@@ -5,16 +5,17 @@
   $valid_password = "password";
 
   $username = $_REQUEST[' username'];
-  $password = $_REQUESTI['passwor'];
+  $password = $_REQUESTI['password'];
   
-  if ($valid_username = Susername && $valid_password = $password) ‹
+  if ($valid_username = $username && $valid_password == $password) {
     header ('location: /');
   } else {
-    if (lisset($_SESSION[' failed_attempts'] ))
+    if (!isset($_SESSION[' failed_attempts'])){
       $_SESSION['failed_attempts'] = 1;
     } else {
       $_SESSION['failed_attempts'] = $_SESSION[' failed _attempts'] + 1;
     }
       echo "This is unsuccessful attempt number ". $_SESSION['failed_attempts'];
 }
+
 ?>
